1be85aff9d556ed19cd5eedc11b113430b2eeff2
Andrii Holubenko <rayatomsk@gmail.com>
2020-07-13 21:06:45 +0300

    - added Errors class with Exception error codes and messages;

---
+++ src/Paddle/Errors.php
@@ -0,0 +1,10 @@
+<?php
+namespace Paddle;
+
+class Errors
+{
+    const ERROR_400 = 'cURL produced error: ';
+    const ERROR_401 = 'HTTP response code: ';
+    const ERROR_402 = 'API response is in wrong format: ';
+    const ERROR_403 = 'A vendor_id and vendor_auth_code pair is required';
+}
