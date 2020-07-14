135490dec71584292c21851b14461d0557083ecf
Andrii Holubenko <rayatomsk@gmail.com>
2020-07-14 15:51:59 +0300

    - added functionality for Paddle Alert API;

--- src/Paddle/Alert.php
+++ src/Paddle/Alert.php
@@ -1,10 +1,35 @@
 <?php
 namespace Paddle;
 
+use Exception;
+
 /**
  * Class Alert
  * @package Paddle
+ * The Paddle Alert API allows you to get the history of all your subscribed webhook alerts.
  */
 class Alert extends Type
 {
+    /**
+     * Retrieve past events and alerts that Paddle has sent to webhooks on your account.
+     * @link https://developer.paddle.com/api-reference/alert-api/webhooks/webhooks
+     *
+     * @param int $page Paginate returned results.
+     * @param int $alertsPerPage Number of webhook alerts to return per page. Returns 10 alerts by default.
+     * @param null|string $queryHead The date and time at which the webhook occurred before (end date).
+     * @param null|string $queryTail The date and time at which the webhook occurred after (start date).
+     *
+     * @return array
+     *
+     * @throws Exception
+     */
+    public function getWebhookHistory($page, $alertsPerPage = 10, $queryHead = null, $queryTail = null)
+    {
+        return $this->api->request('POST', '/2.0/alert/webhooks', [
+            'page'              => $page,
+            'alerts_per_page'   => $alertsPerPage,
+            'query_head'        => $queryHead,
+            'query_tail'        => $queryTail
+        ]);
+    }
 }
